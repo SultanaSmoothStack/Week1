a6b9ed311f86f93b2c3a62a0ce388c3b5ea77b51
SultanaK <kr.shampa@yahoo.com>
2021-02-23 13:07:34 -0600

    adding input validation to number guess game

--- Week1/day1/assignment2/src/com/sultana/Main.java
+++ Week1/day1/assignment2/src/com/sultana/Main.java
@@ -5,43 +5,48 @@ public class Main {
 
     public static void main(String[] args) {
         int secretNumber;
+
         // create a random number from 1 to 100
-        secretNumber = (int) ( Math.random()* 100 );
+        secretNumber = (int) (Math.random() * 100);
         //System.out.println(secretNumber);
 
-        Scanner userInput = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int guessedNumber;
         int numberOfTries = 0;
-        final int maxTries = 3 ;
+        final int maxTries = 3;
 
-        for (int i = 0; i <maxTries ; i++) {
+        for (int i = 0; i < maxTries; i++) {
             System.out.println("Enter your Number between 1 to 100:  ");
-            guessedNumber = userInput.nextInt();
-            /*while(!userInput.hasNextInt()){
-                System.out.println("This is not a valid input");
-                userInput.next();
-            }*/
+            // Input validation
+            while (!scanner.hasNextInt()) {
+                String input = scanner.next();
+                System.out.printf("\"%s\" is not a valid number. Enter a number between 1 to 100\n", input);
+            }
+            guessedNumber = scanner.nextInt();
+
             numberOfTries++;
             System.out.println("Your guess is: " + guessedNumber);
 
-            if (guessedNumber == secretNumber){
+            if (guessedNumber == secretNumber) {
                 System.out.println(" Your guess is correct. Congratulations!!! ");
                 break;
-            }
-            else if (guessedNumber < secretNumber)
+            } else if (guessedNumber < 0 || guessedNumber > 100) {
+                System.out.println(" your guess is out of range");
+            } else if (guessedNumber < secretNumber) {
                 System.out.println(" Your guess number is smaller than the secrect number");
-            else System.out.println(" your guess is greater than the number");
+            } else {
+                System.out.println(" your guess is greater than the number");
             }
-
-            if(maxTries == numberOfTries){
-                System.out.println( " ");
-                System.out.println(" You loose. ") ;
-                System.out.println("The number was: " + secretNumber);
-
-                System.out.println(" You tries " + numberOfTries +" times");
+        }
+        // checking maximum tries
+        if (maxTries == numberOfTries) {
+            System.out.println(" ");
+            System.out.println(" You loose. ");
+            System.out.println("The number was: " + secretNumber);
+            System.out.println(" You tries " + numberOfTries + " times");
         }
 
     }
 
-    }
+}
 
