4dc5d9fb39ac6ae7d1c7c26c596bce48923a6ac8
SultanaK <kr.shampa@yahoo.com>
2021-02-27 16:20:29 -0600

    finished basic lambda with all the tests

--- Week1/day5/src/main/java/com/sultana/BasicLambda.java
+++ Week1/day5/src/main/java/com/sultana/BasicLambda.java
@@ -1,13 +1,16 @@
 package main.java.com.sultana;
-
 import java.util.Arrays;
+import java.util.Comparator;
+
 public class BasicLambda {
 
     public static void main(String[] args) {
         BasicLambda basicLambda = new BasicLambda();
-        String[] names = new String[]{"Sultana","Soha", "Zoha","Zareeb", "Javed"};
+        String[] names = new String[]{"Sultana","Soha", "Zoha","Zareeb", "Javed","Ezan"};
         basicLambda.sortByAscending(names);
         basicLambda.sortByDescending(names);
+        basicLambda.sortByAlphabeticallyByFirst(names);
+        basicLambda.sortByCharacter(names);
     }
 
     public void sortByDescending(String[] names) {
@@ -15,7 +18,7 @@ public class BasicLambda {
             return;
         }
         Arrays.sort(names, (str1, str2) -> str2.length() - str1.length());
-        System.out.println("Sorting a names array in descending order  using Lamda: \n" + Arrays.deepToString(names));
+        System.out.println("Sorting a names array in descending order: \n" + Arrays.deepToString(names));
     }
 
     public void sortByAscending(String[] names) {
@@ -23,6 +26,22 @@ public class BasicLambda {
             return;
         }
         Arrays.sort(names, (str1, str2) -> str1.length() - str2.length());
-        System.out.println("Sorting a names array in ascending order  using Lamda: \n" + Arrays.deepToString(names));
+        System.out.println("Sorting a names array in ascending order: \n" + Arrays.deepToString(names));
+    }
+    public void sortByAlphabeticallyByFirst(String[] names){
+        if (names == null) {
+            return;
+        }
+        Arrays.sort(names, (str1, str2) -> str1.charAt(0) - str2.charAt(0));
+        //Arrays.asList(names).forEach(System.out::println);
+        System.out.println("Sorting a names array alphabetically by the first character: \n" + Arrays.deepToString(names) );
+    }
+    public void sortByCharacter(String[] names){
+        if (names == null) {
+            return;
+        }
+        Arrays.sort(names, Comparator.comparingInt(a -> (a.contains("E") || a.contains("e") ? 0 : 1)));
+        for(String name : names)
+            System.out.println("Strings that contain “e” first, everything else second: \n"+ Arrays.deepToString(names));
     }
 }
--- Week1/day5/src/test/java/com/sultana/BasicLambdaTest.java
+++ Week1/day5/src/test/java/com/sultana/BasicLambdaTest.java
@@ -9,20 +9,36 @@ public class BasicLambdaTest {
 
     @Test
     public void testSortByDescending() {
-        String[] names = new String[]{"Sultana","Sohaa", "Zoha","Zareeb", "Javed"};
+        String[] names = new String[]{"Sultana","Sohaa", "Zoha","Zareeb", "Javed","Ezan"};
         BasicLambda basicLambda = new BasicLambda();
         basicLambda.sortByDescending(names);
         assertEquals(names[0], "Sultana");
-        assertEquals(names[4], "Zoha");
+        assertEquals(names[5], "Ezan");
     }
 
     @Test
     public void testSortByAscending() {
-        String[] names = new String[]{"Sultana","Sohaa", "Zoha","Zareeb", "Javed"};
+        String[] names = new String[]{"Sultana","Sohaa", "Zoha","Zareeb", "Javed","Ezan"};
         BasicLambda basicLambda = new BasicLambda();
         basicLambda.sortByAscending(names);
         assertEquals(names[0], "Zoha");
-        assertEquals(names[4], "Sultana");
+        assertEquals(names[5], "Sultana");
+    }
+    @Test
+    public void testSortByAlphabeticallyByFirst() {
+        String[] names = new String[]{"Sultana","Sohaa", "Zoha","Zareeb", "Javed","Ezan"};
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAlphabeticallyByFirst(names);
+        assertEquals(names[0], "Ezan");
+        assertEquals(names[5], "Zareeb");
+    }
+@Test
+    public void testSortByCharacter() {
+        String[] names = new String[]{"Sultana","Sohaa", "Zoha","Zareeb", "Javed","Ezan"};
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAlphabeticallyByFirst(names);
+        assertEquals(names[0], "Ezan");
+        assertEquals(names[5], "Zareeb");
     }
 
     @Test
@@ -33,6 +49,44 @@ public class BasicLambdaTest {
 
     @Test
     public void testSortByAscendingForEmpty() {
+        String[] names = new String[]{};
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAscending(names);
+        assertEquals(names.length, 0);
+    }
+    @Test
+    public void testSortByDescendingForNull() {
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAscending(null);
+    }
+
+    @Test
+    public void testSortByAlphabeticallyByFirstForEmpty() {
+        String[] names = new String[]{};
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAscending(names);
+        assertEquals(names.length, 0);
+    }
+    @Test
+    public void testSortByAlphabeticallyByFirstForNull() {
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAscending(null);
+    }
+
+    @Test
+    public void testSortByDescendingForEmpty() {
+        String[] names = new String[]{};
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAscending(names);
+        assertEquals(names.length, 0);
+    }@Test
+    public void testSortByCharacterFirstForNull() {
+        BasicLambda basicLambda = new BasicLambda();
+        basicLambda.sortByAscending(null);
+    }
+
+    @Test
+    public void testSortByCharacterForEmpty() {
         String[] names = new String[]{};
         BasicLambda basicLambda = new BasicLambda();
         basicLambda.sortByAscending(names);
