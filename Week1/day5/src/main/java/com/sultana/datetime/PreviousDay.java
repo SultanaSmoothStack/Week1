969c23ec9ae02fc9ce40b8bd1e9f8de37c97be66
SultanaK <kr.shampa@yahoo.com>
2021-02-28 11:55:05 -0600

    Implement month length in a year with Date-time API

--- Week1/day5/src/main/java/com/sultana/datetime/PreviousDay.java
+++ Week1/day5/src/main/java/com/sultana/datetime/PreviousDay.java
@@ -4,13 +4,16 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.temporal.TemporalAdjusters;
-import java.util.Date;
+
 public class PreviousDay {
     //private static Object date;
 
     public static void main(String[] args) {
+        previousDay();
+    }
+
+    public static void previousDay() {
         LocalDate date = LocalDate.of(2021,Month.FEBRUARY,28);
-        LocalDate previous = date.minus();
         LocalDate previous = date.with(TemporalAdjusters.previous(DayOfWeek.SUNDAY));
 
         System.out.println(previous);
